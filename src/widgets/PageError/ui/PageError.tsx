63591ffc352931e0f391b33b71d9d87357810a77
IgorDCat <powerwolf891028@gmail.com>
2022-09-25 23:33:48 +0400

    added error boundary, page error

---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import cls from "./PageError.module.scss";
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTranslation} from "react-i18next";
+import {Button} from "shared/ui/Button/Button";
+
+export const PageError = () => {
+    const {t} = useTranslation();
+    const reloadPage = () => {
+        location.reload()
+    }
+    return (
+        <div className={classNames(cls.PageError)}>
+            <p>{t("Error: Something went wrong")}</p>
+            <Button onClick={reloadPage}>{t("Reload page")}</Button>
+        </div>
+    );
+}
